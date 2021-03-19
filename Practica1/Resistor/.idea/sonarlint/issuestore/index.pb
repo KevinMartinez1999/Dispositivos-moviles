29312c29ef6064bc1ddd42ae3e4822325930f3c2
JDRIOS <judari_66@hotmail.com>
2021-03-19 08:47:47 -0500

    lab3

---
+++ Practica1/Resistor/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+@
+app/build.gradle,f\4\f4a01d6a4fcb971362ec00a83903fd3902f52164
+i
+9app/src/main/java/com/juanrios66/resistor/MainActivity.kt,2\4\245157f0cfddb0686996272863bdaa5fd353ca81
---
+++ Practica1/Resistor/app/src/main/java/com/juanrios66/resistor/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.juanrios66.resistor
+
+import android.os.Bundle
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemSelectedListener
+import androidx.appcompat.app.AppCompatActivity
+import com.juanrios66.resistor.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainBinding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+
+        val arraycolors = resources.getStringArray(R.array.color)
+        val colors = resources.getIntArray(R.array.colrs)
+        
+
+
+        mainBinding.spinner.onItemSelectedListener = object : OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?) {
+                for (num in 0..10 step 1 ){
+                    if(mainBinding.spinner.selectedItem.toString() == arraycolors[num]){
+                        mainBinding.spinner.setBackgroundColor(colors[num])
+                    }else{
+                    }
+                }
+            }
+        }
+
+        mainBinding.buttonCalc.setOnClickListener{
+            for (num in 0..10 step 1 ){
+                if(mainBinding.spinner.selectedItem.toString() == arraycolors[num]){
+                    mainBinding.spinner.setBackgroundColor(colors[num])
+                }else{
+                }
+            }
+        }
+
+    }
+}
